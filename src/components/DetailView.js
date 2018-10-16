7f78a7fd0bdd8faf7cf8dc952a06d95fb55504f2
Nicholas Kennedy <nicholaskennedy@ACD.INT>
2018-10-16 15:35:16 -0700

    updates to detail view

--- src/components/DetailView.js
+++ src/components/DetailView.js
@@ -4,6 +4,16 @@ import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import MediaSlider from './MediaSlider';
+import firebase from '../firebase.js';
+import Geocode from "react-geocode";
+import IconButton from '@material-ui/core/IconButton';
+import EmailIcon from '@material-ui/icons/Email';
+
+const db = firebase.firestore();
+const settings = {timestampsInSnapshots: true};
+db.settings(settings);
 
 const styles = theme => ({
   '@global': {
@@ -34,26 +44,84 @@ const styles = theme => ({
     paddingBottom: theme.spacing.unit * 4,
     fontWeight: 'initial',
   },
+  paper: {
+    paddingBottom: theme.spacing.unit,
+  },
 });
 
-function DetailView(props) {
-    const { classes } = props;
+class DetailView extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        data: [],
+        location: "",
+      };
+      this.getLocation = this.getLocation.bind(this);
+    }
+    getLocation = (lat, long) => {
+      lat = lat.toString()
+      long = long.toString()
+      Geocode.fromLatLng(lat, long).then(
+          response => {
+              const address = response.results[4].formatted_address;
+              this.setState({
+                  location: address
+              });
+          },
+          error => {
+              //parse and return error
+              console.error(error);
+          }
+        );
+    }
+    componentDidMount() {
+      db.collection('Posts').doc(this.props.match.params.listingId).get()
+          .then((querySnapshot) => {
+          let data = querySnapshot.data();
+          this.setState({
+              data: data
+          }, () => {
+            this.getLocation(this.state.data.location.latitude, this.state.data.location.longitude);
+          });
+      });
+    }
+    render() {
+    const { classes, theme } = this.props;
     return (
       <React.Fragment>
       <div className={classNames(classes.layout)}>
-        <h4 className={classNames(classes.tagline)}>Detail View - {props.match.params.listingId}</h4>
         <div className={classes.root}>
           <Grid container spacing={16}>
             <Grid item xs={12} sm={8}>
-              <Paper className={classes.paper}>Slider</Paper>
+              <Paper className={classes.paper}><MediaSlider document_id={this.props.match.params.listingId} /></Paper>
             </Grid>
             <Grid item xs={12} sm={4}>
-              <Paper className={classes.paper}>Info</Paper>
-              <Paper className={classes.paper}>Contact</Paper>
+              <Paper className={classes.paper} elevation={0}>
+                <Typography variant="title" component="h2">
+                  {this.state.data.manufacturer + " " + this.state.data.length + "'"}
+                </Typography>
+                <Typography variant="subheading" component="h3" gutterBottom>
+                  { this.state.boat_name != null ? (this.state.location) : (this.state.data.boat_name + ", " + this.state.location)}
+                </Typography>
+                <Typography variant="headline" component="h4">
+                  {"$" + this.state.data.asking_price}
+                </Typography>
+              </Paper>
+              <Paper className={classes.paper} elevation={0}>
+                <IconButton className={classes.button} aria-label="Email">
+                  <EmailIcon fontSize="small"/>
+                </IconButton>
+              </Paper>
+              <Paper className={classes.paper} elevation={0}>
+                <Typography variant="caption" component="p">
+                  {"Declared by owner to be in " + this.state.data.condition + " condition overall."}
+                </Typography>
+                <Typography variant="body1" component="p">
+                  {this.state.data.description}
+                </Typography>
+              </Paper>
               <Paper className={classes.paper}>Map</Paper>
             </Grid>
-          </Grid>
-          <Grid container spacing={16}>
             <Grid item xs={12} sm={4}>
               <Paper className={classes.paper}>Description</Paper>
             </Grid>
@@ -68,6 +136,7 @@ function DetailView(props) {
       </div>
       </React.Fragment>
     );
+  }
 }
 
 DetailView.propTypes = {
