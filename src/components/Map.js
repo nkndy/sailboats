bce747072c3210ce2569d80a01d34a68608c2f1d
Nicholas Kennedy <nicholaskennedy@ACD.INT>
2018-10-17 15:24:47 -0700

    working geocode with mpa render

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,6 +1,14 @@
 import React from 'react';
+import Geocode from "react-geocode";
+import GoogleMapReact from 'google-map-react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
 import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
 
+// // set Google Maps Geocoding API for purposes of quota management. Its optional but recommended.
+Geocode.setApiKey("AIzaSyA56WfgzuCmL-Qtji_kytS50CCk9QJpmIE");
+// Enable or disable logs. Its optional.
+// Geocode.enableDebug();
 
 const containerStyle = {
   height: "100%",
