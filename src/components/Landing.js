1af2ee98ba46d43dfc32a6138ed5f2e92bda7a22
nkndy_ <nicholas.stuart.kennedy@gmail.com>
2018-10-17 21:45:58 -0700

    add landing bg

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -6,6 +6,7 @@ import Button from '@material-ui/core/Button';
 import NearMe from '@material-ui/icons/NearMe';
 import { withStyles } from '@material-ui/core/styles';
 import { NavLink } from "react-router-dom";
+import bgImage from '../assets/img/headerbg.png';
 
 const styles = theme => ({
   '@global': {
@@ -15,24 +16,26 @@ const styles = theme => ({
   },
   layout: {
     width: 'auto',
-    marginLeft: theme.spacing.unit * 3,
-    marginRight: theme.spacing.unit * 3,
+    paddingLeft: theme.spacing.unit * 1,
+    paddingRight: theme.spacing.unit * 1,
     paddingTop: theme.spacing.unit * 3,
     marginTop: '70px',
     textAlign: 'center',
     [theme.breakpoints.up(900 + theme.spacing.unit * 3 * 2)]: {
       width: 900,
+      paddingLeft: theme.spacing.unit * 3,
+      paddingRight: theme.spacing.unit * 3,
       marginLeft: 'auto',
       marginRight: 'auto',
     },
     [theme.breakpoints.up(550 + theme.spacing.unit * 3 * 2)]: {
+      paddingLeft: theme.spacing.unit * 2,
+      paddingRight: theme.spacing.unit * 2,
       marginTop: '82px',
     },
   },
   tagline: {
     textAlign: 'center',
-    paddingBottom: theme.spacing.unit * 4,
-    fontWeight: 'initial',
   },
   landingSection: {
     marginBottom: theme.spacing.unit * 4,
@@ -46,16 +49,27 @@ const styles = theme => ({
   extendedIcon: {
     marginRight: theme.spacing.unit * 1,
   },
+  landingTaglineContainer: {
+    paddingTop: theme.spacing.unit * 17,
+    paddingBottom: theme.spacing.unit * 15,
+    backgroundImage: `url(${bgImage})`,
+    backgroundPosition: 'right bottom',
+    [theme.breakpoints.up(900 + theme.spacing.unit * 3 * 2)]: {
+
+    },
+  },
 });
 
 function Landing(props) {
     const { classes } = props;
     return (
       <React.Fragment>
-      <div className={classNames(classes.layout)}>
+      <div className={classNames(classes.landingTaglineContainer)}>
         <Typography className={classNames(classes.tagline)}>
           Find your next adventure
         </Typography>
+      </div>
+      <div className={classNames(classes.layout)}>
         <div className={classNames(classes.landingSection)}>
           <Typography variant="subheading" gutterBottom>
             This is some lorem ipsum about how we are a global sailboat marketplace.
